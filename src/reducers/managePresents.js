dcab7797e1c0c8541e1b93fa33da2aaae0c0feea
no-relation <eddiec76@gmail.com>
2019-01-03 15:41:06 -0600

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
+// action = {
+//     type: "INCREASE"
+// }
+
 export function managePresents(state, action){
+    switch (action.type) {
+        case "INCREASE":
+            return { numberOfPresents: state.numberOfPresents + 1}
+        default:
+            return state
+
+    }
 }
