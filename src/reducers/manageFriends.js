b4825f184c3256d1bdaa5cef1e36d599b4143cb6
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2019-01-04 09:57:48 -0600

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE FRIEND":
+      return {
+        friends: state.fiends.filter(friend => friend !== action.payload)
+      };
+    default:
+      return state;
+  }
 }
